9ea61c3d75a2719241c7df46ec9ed52260160e8a
sajimpk <sajimpk520@gmail.com>
2024-08-11 15:55:15 +0600

    update

--- Include/scripts/groovy/operations/str.groovy
+++ Include/scripts/groovy/operations/str.groovy
@@ -1,3 +1,4 @@
+package operations
 import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
@@ -43,12 +44,22 @@ import cucumber.api.java.en.When
 
 
 
-class steps {
+class str {
 	/**
 	 * The step definitions below match with Katalon sample Gherkin steps
 	 */
-	@Given("I want to write a step with")
+	@Given("I want to write a step with (.*)")
 	def I_want_to_write_a_step_with_name(String name) {
-		println ("helo")
+		println name
+	}
+
+	@When("I check for the (\\d+) in step")
+	def I_check_for_the_value_in_step(int value) {
+		println value
+	}
+
+	@Then("I verify the (.*) in step")
+	def I_verify_the_status_in_step(String status) {
+		println status
 	}
 }
